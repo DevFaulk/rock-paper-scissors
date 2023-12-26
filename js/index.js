79a6a2d553baa86066a8e418209b5daff153ddd6
DevFaulk <faulkdev@gmail.com>
2023-12-26 18:04:09 -0500

    change js structure

--- js/index.js
+++ js/index.js
@@ -1,18 +1,30 @@
 "use strict";
+
+function getComputerChoice() {
+  let randomChoice = Math.floor(Math.random() * 10);
+  if (randomChoice < 3) {
+    return "Rock";
+  } else if (randomChoice >= 3 && randomChoice <= 6) {
+    return "Paper";
+  } else if (randomChoice > 6) {
+    return "Scissors";
+  }
+}
+
 function displayWin(playerChoice, computerChoice) {
   return `You chose: ${playerChoice}
-    Computer chose: ${computerChoice}
-    Result: You win!`;
+      Computer chose: ${computerChoice}
+      Result: You win!`;
 }
 function displayLose(playerChoice, computerChoice) {
   return `You chose: ${playerChoice}
-    Computer chose: ${computerChoice}
-    Result: You lose.`;
+      Computer chose: ${computerChoice}
+      Result: You lose.`;
 }
 function displayTie(playerChoice, computerChoice) {
   return `You chose: ${playerChoice}
-    Computer chose: ${computerChoice}
-    Result: Tie!`;
+      Computer chose: ${computerChoice}
+      Result: Tie!`;
 }
 
 function displayGameResult(playerChoice) {
@@ -31,17 +43,6 @@ function displayGameResult(playerChoice) {
     (computerChoice === "Scissors" && playerChoice === "Rock")
   ) {
     displayWin(playerChoice, computerChoice);
-  }
-}
-
-function getComputerChoice() {
-  let randomChoice = Math.floor(Math.random() * 10);
-  if (randomChoice < 3) {
-    return "Rock";
-  } else if (randomChoice >= 3 && randomChoice <= 6) {
-    return "Paper";
-  } else if (randomChoice > 6) {
-    return "Scissors";
   }
 }
 
